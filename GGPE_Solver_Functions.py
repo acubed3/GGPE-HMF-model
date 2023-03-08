c8e86721bb3915497fef5c9ff05dbb3c34c9694e
acubed3 <aleksandrov.aa@phystech.edu>
2023-03-08 13:27:44 +0300

    update function

--- GGPE_Solver_Functions.py
+++ GGPE_Solver_Functions.py
@@ -8,62 +8,75 @@ from scipy.linalg import lu
 from tqdm import tqdm
 
 
-def magnetization(A_vec):
-    '''For given vector compute mangetization'''
-    conjugate_A = np.conjugate(A_vec)
-    M_list = [conjugate_A[i] * InitialA[i + 1] for i in range(0, 2 * kMax)]
-    M_list = np.array(M_list)
-    M = np.sum(M_list)
-    return M
+def magnetization(a_vec):
+    """For given vector compute magnetization"""
+    conj_a_vec = np.conjugate(a_vec)
+    roll_a_vec = np.roll(a_vec, -1, axis=0)
+    roll_a_vec[-1] = 0
+    m_vec = conj_a_vec * roll_a_vec
+    m = np.sum(m_vec)
+    return m
 
 
-def pseudo_hamiltonian(chi, A_vec, kMax):
-    '''Construct tridiagonal matrix'''
+def pseudo_hamiltonian(chi, a_vec, k_max):
+    """Construct tridiagonal matrix"""
     from scipy.sparse import diags
-    KineticTerm = chi ** 2 / 2 * np.arange(-kMax, kMax + 1, 1) ** 2
-    Mag = magnetization(A_vec) * np.ones(len(A_vec) - 1)
-    ConjMag = np.conjugate(Mag) * np.ones(len(A_vec) - 1)
-    H_matrix = scipy.sparse.diags([KineticTerm, -1 / 2 * Mag, -1 / 2 * ConjMag], [0, -1, 1]).toarray()
-    return H_matrix
+    kinetic_term = chi ** 2 / 2 * np.arange(-k_max, k_max + 1, 1) ** 2
+    mag = magnetization(a_vec) * np.ones(len(a_vec) - 1)
+    conj_mag = np.conjugate(mag) * np.ones(len(a_vec) - 1)
+    h_matrix = scipy.sparse.diags([kinetic_term, -1 / 2 * mag, -1 / 2 * conj_mag], [0, -1, 1]).toarray()
+    return h_matrix
 
 
-def evolution_operator(chi, A_vec, kMax, dT):
-    '''Construct evolution opertator'''
-    H_matrix = pseudo_hamiltonian(chi, A_vec, kMax)
-    U = np.identity(len(H_matrix)) - 1j * dT * H_matrix
-    return U
+def evolution_operator(chi, a_vec, k_max, t_step):
+    """Construct evolution operator"""
+    h_matrix = pseudo_hamiltonian(chi, a_vec, k_max)
+    evolution_matrix = np.identity(len(h_matrix)) - 1j * t_step * h_matrix
+    return evolution_matrix
 
 
-def equation_solver(A_prev, A_prev_T):
-    '''Construct operator in LHS that acts on unknown vector A and known RHS'''
-    left_U = evolution_operator(chi, A_prev, kMax, -dT / 2)
-    right_U = evolution_operator(chi, A_prev_T, kMax, dT / 2)
-    lhs = left_U
-    rhs = np.dot(right_U, A_prev_T)
-    # LHS_L, LHS_U = lu(LHS, permute_l=True)
-    # U_A = solve(LHS_L, RHS)
-    A = solve(lhs, rhs)
+def equation_solver(a_prev, a_prev_t):
+    """Construct operator in LHS that acts on unknown vector A and known RHS"""
+    left_u = evolution_operator(chi, a_prev, k_max, -t_step / 2)
+    right_u = evolution_operator(chi, a_prev, k_max, t_step / 2)
+    lhs = left_u
+    rhs = np.dot(right_u, a_prev_t)
+    lhs_l, lhs_u = lu(lhs, permute_l=True)
+    u_a = solve(lhs_l, rhs)
+    a = solve(lhs, rhs)
+    return a
 
-    return A
+
+def converge_approx(a_prev, a_prev_t):
+    while True:
+        a_new_approx = a_prev
+        a_prev = equation_solver(a_prev, a_prev_t)
+        if np.allclose(a_new_approx, a_prev, rtol=1e-7):
+            a_new_t = a_prev
+            break
+    return a_new_t
 
 
-def time_evolution(InitialA, Tmax):
+def time_evolution(initial_a, t_max):
     '''Perform evolutioon from initial vector InitialA until Tmax will be achieved'''
-    U0 = evolution_operator(chi, InitialA, kMax, dT)
-    A_prev = np.dot(U0, InitialA)
-    A_prev_T = InitialA
+    u0 = evolution_operator(chi, initial_a, k_max, t_step)
+    a_prev = np.dot(u0, initial_a)
+    a_prev_t = initial_a
     mode_vals = []
-    for _ in tqdm(np.arange(dT, Tmax + dT, dT)):
-        RightU_T = evolution_operator(chi, A_prev_T, kMax, dT / 2)
-        RHS_T = np.dot(RightU_T, A_prev_T)
-        while True:
-            ANewApprox = equation_solver(A_prev, A_prev_T)
-            LeftU_New = evolution_operator(chi, ANewApprox, kMax, -dT / 2)
-            LHS_New = np.dot(LeftU_New, ANewApprox)
-            Residual = LHS_New - RHS_T
-            if np.linalg.norm(Residual) / np.linalg.norm(A_prev_T) < 10 ** (-7):
-                A_prev = ANewApprox
-                break
-        A_prev_T = A_prev
-        mode_vals.append(A_prev_T)
+    for i in tqdm(np.arange(t_step, t_max + t_step, t_step)):
+        #         #RightU_T = EvolutionOperator(chi, A_prev_T, kMax, dT/2)
+        #         #RHS_T = np.dot(RightU_T, A_prev_T)
+        #         while True:
+        #             A_new_approx = A_prev
+        #             A_prev = EquationSolver(A_prev, A_prev_T)
+        #             #LeftU_New = EvolutionOperator(chi, A_prev, kMax, -dT/2)
+        #             #LHS_New = np.dot(LeftU_New, A_prev)
+        #             #Residual = LHS_New - RHS_T
+        #             if np.allclose(A_new_approx, A_prev, rtol=1e-7):
+        #                 A_prev_T = A_prev
+        #                 break
+        a_prev_t = converge_approx(a_prev, a_prev_t)
+        mode_vals.append(a_prev_t)
+        u0 = evolution_operator(chi, a_prev_t, k_max, t_step)
+        a_prev = np.dot(u0, a_prev_t)
     return np.array(mode_vals)
